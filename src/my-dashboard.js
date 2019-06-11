2dfa761e896d72ab946c7ec544d824a3da563e8c
User1 <User1@LP-CNU243C03H.HCLT.CORP.HCL.IN>
2019-06-11 20:12:59 +0530

    UI Integration Done

--- src/my-dashboard.js
+++ src/my-dashboard.js
@@ -1,4 +1,4 @@
-import {PolymerElement, html} from '@polymer/polymer/polymer-element.js';
+import { PolymerElement, html } from '@polymer/polymer/polymer-element.js';
 import '@polymer/iron-input/iron-input.js';
 import '@polymer/paper-card/paper-card.js';
 import '@polymer/paper-button/paper-button.js';
@@ -6,9 +6,12 @@ import '@polymer/iron-form/iron-form.js';
 import '@polymer/paper-input/paper-input.js';
 import '@polymer/iron-autogrow-textarea/iron-autogrow-textarea.js';
 import '@vaadin/vaadin-grid/vaadin-grid.js';
+import '@polymer/paper-dropdown-menu/paper-dropdown-menu.js';
+import '@polymer/paper-item/paper-item.js';
+import '@polymer/paper-listbox/paper-listbox.js';
 
 class EditableNameTag extends PolymerElement {
-  static get properties () {
+  static get properties() {
     return {
       owner: {
         type: String,
@@ -20,145 +23,176 @@ class EditableNameTag extends PolymerElement {
         //value: "http://10.117.189.210:9090/app"
         value: "http://10.117.189.210:8090/app"
       },
-      showLloggedInUserDetails:{//showLloggedInUserDetails,showTransferPage,showAccountDetailsPage
-        type:Boolean,
-        value:true
+      showLloggedInUserDetails: {//showLloggedInUserDetails,showTransferPage,showAccountDetailsPage
+        type: Boolean,
+        value: true
       },
-      showTransferPage:{
-        type:Boolean,
-        value:false
+      showTransferPage: {
+        type: Boolean,
+        value: false
       },
-      showAccountDetailsPage:{
-        type:Boolean,
-        value:false
+      showAccountDetailsPage: {
+        type: Boolean,
+        value: false
       },
-      loggedInUsrAccountDetails:{
-        type:Object,
-        value:{}
+      loggedInUsrAccountDetails: {
+        type: Object,
+        value: {}
 
       },
-      loggedInUsraccountDetailNTrans:{
-        type:Array,
-        value:[]
+      loggedInUsraccountDetailNTrans: {
+        type: Array,
+        value: []
 
       },
-      loggedInUsraccountOnce:{
-        type:Object,
-        value:{}
+      loggedInUsraccountOnce: {
+        type: Object,
+        value: {}
 
       },
-      toAccountListArray:{
-        type:Array,
-        value:[]
+      toAccountListArray: {
+        type: Array,
+        value: []
       }
     };
   }
 
-  connectedCallback(){
+  connectedCallback() {
     super.connectedCallback();
-    localStorage.apiname="transfer";
+    localStorage.apiname = "transfer";
     this.getAccount();
-   
+
   }
-  getToAccountList(){
-    localStorage.apiname="toAccountList";
+  getToAccountList() {
+
+    console.log("getToAccountList")
+    // localStorage.apiname=="transfer";
+    localStorage.apiname = "toAccountList";
     let ajaxRef = this.$.ajax;
     ajaxRef.method = "get";
-    ajaxRef.url = `${this.baseUrl}/beneficiary/${localStorage.accountId }`,
-    ajaxRef.contentType = "application/json"
+    ajaxRef.url = `${this.baseUrl}/beneficiary/${localStorage.accountId}`,
+      ajaxRef.contentType = "application/json"
     ajaxRef.generateRequest();
   }
-  getAccount(){
+  getAccount() {
     let ajaxRef = this.$.ajax;
     ajaxRef.method = "get";
-    ajaxRef.url = `${this.baseUrl}/account/${localStorage.accountId }`,
-    ajaxRef.contentType = "application/json"
+    ajaxRef.url = `${this.baseUrl}/account/${localStorage.accountId}`,
+      ajaxRef.contentType = "application/json"
     ajaxRef.generateRequest();
   }
   _onError(event) {
     const req = event.detail; // iron-request
     var result = req.response;
-    console.log("Result:"+JSON.stringify(result));
+    console.log("Result:" + JSON.stringify(result));
     console.log('status code', req.status);
     console.log('status text', req.statusText);
     // I think one of those two would be what you're looking for.
     //console.log(event.detail.response);
-   // alert(result.message);
-    alert( "Something Went Wrong! ");
-   
-   
+    // alert(result.message);
+    alert("Something Went Wrong! ");
+
+
   }
-  _onResponse(event,request) {
-    const result = event.detail.response;  
-    if(localStorage.apiname=="transfer"){
+  _onResponse(event, request) {
+    console.log(localStorage.apiname)
+    const result = event.detail.response;
+    if (localStorage.apiname == "transfer") {
       this.loggedInUsrAccountDetails = result;
-      if(this.loggedInUsraccountDetailNTrans){
+      if (this.loggedInUsraccountDetailNTrans) {
         alert("Successfully Tranaction Done");
       }
 
-    }else if(localStorage.apiname=="accountDetail"){
+    } else if (localStorage.apiname == "accountDetail") {
       //localStorage.apiname=="accountDetail"
       this.loggedInUsraccountDetailNTrans = result;
-      console.log("data:"+result);
-      this.loggedInUsraccountOnce =  result[0].fromAccount;
+      console.log("data:" + result);
+      this.loggedInUsraccountOnce = result[0].fromAccount;
       
-    }else if(localStorage.apiname=="toAccountList"){
+    } else if (localStorage.apiname == "toAccountList") {
       //localStorage.apiname=="accountDetail"
       this.toAccountListArray = result;
+    }else if (localStorage.apiname == "transferDetails") {
+      //localStorage.apiname=="accountDetail"
+      //this.toAccountListArray = result;
+      alert("Successfully Transferd");
+    } else if (localStorage.apiname == "transferDeta") {
+      alert("Transaction Id: " + result.transactionId + "\n Transcation Amount: " + result.transferAmount + "\n From Account: " + result.fromAccount.accountHolderName + "\n To Account : " + result.toAccount.accountHolderName + "\n Transfer Type: " + result.transferType);
+      console.log(result)
     }
-  
+
   }
-  _transMoney(){
-   // alert("_transMoney");
-    this.showLloggedInUserDetails =false; 
+  _transMoney() {
+    // alert("_transMoney");
+    console.log("_transMoney")
+    this.getToAccountList();
+    this.showLloggedInUserDetails = false;
     this.showTransferPage = true;
     console.log('_transMoney');
-    localStorage.apiname=="transfer";
-    
-    
-  //  this.loggedInUsraccountDetail = result;
+
+
+
+    //  this.loggedInUsraccountDetail = result;
 
   }
 
-  _accountDetails(){
+  _handleChangeSelect(event) {
+    console.log(event.target.selectedItem)
+    this.fromAccountNumber = event.target.selectedItem.value;
+  }
+  _accountDetails() {
     //showLloggedInUserDetails,showTransferPage,showAccountDetailsPage
-    localStorage.apiname="accountDetail"
+    localStorage.apiname = "accountDetail"
     this.showLloggedInUserDetails = false;
-    this.showTransferPage =false;
+    this.showTransferPage = false;
     this.showAccountDetailsPage = true;
-    
+
     //call 
     let ajaxRef = this.$.ajax;
     ajaxRef.method = "get";
     ajaxRef.url = `${this.baseUrl}/accountDetail/${localStorage.accountId}`,
-   // ajaxRef.body = jsonBody;
-    ajaxRef.contentType = "application/json"
+      // ajaxRef.body = jsonBody;
+      ajaxRef.contentType = "application/json"
     ajaxRef.generateRequest();
 
 
   }
-  _currentTranactionDetails(event){
-    
-   // alert(event.target.id);
+  _currentTranactionDetails(event) {
+   // console.log(_currentTranactionDetails)
+    localStorage.apiname = "transferDeta";
+    console.log(JSON.parse(event.target.dataset.item).transactionId);
+    let id= JSON.parse(event.target.dataset.item).transactionId;
+    let ajaxRef = this.$.ajax;
+    ajaxRef.method = "get";
+    ajaxRef.url = `${this.baseUrl}/transferDetails/${id}`;
+    ajaxRef.generateRequest();
+    console.log(localStorage.apiname)
+    // alert(event.target.id);
   }
-  _makePayment(){
-   // {fromAccount,toAccount,comment,transferAmount}
+  _makePayment() {
+    localStorage.apiname = "transfer";
+    console.log(this.fromAccountNumber, localStorage.accountId)
+    // {fromAccount,toAccount,comment,transferAmount}
     const jsonBody = {
-      fromAccount: this.fromAccount,
-      toAccount:this.toAccount,
-      comment:this.comment,
-      transferAmount:this.transferAmount
+      fromAccount: localStorage.accountId,
+      toAccount: this.fromAccountNumber,
+      comment: this.comment,
+      transferAmount: this.transferAmount
     }
     let ajaxRef = this.$.ajax;
     ajaxRef.method = "put";
     ajaxRef.url = `${this.baseUrl}/transfer`,
-    ajaxRef.body = jsonBody;
+      ajaxRef.body = jsonBody;
     ajaxRef.contentType = "application/json"
     ajaxRef.generateRequest();
 
-   // alert("_makePayment");
+    // alert("_makePayment");
   }
-  static get template () {
+  _getAccountDetails() {
+    console.log(localStorage.accountId)
+    return localStorage.accountId;
+  }
+  static get template() {
     return html`
     <custom-style>
   <style>
@@ -230,13 +264,25 @@ class EditableNameTag extends PolymerElement {
   <paper-card heading="Money Transfer" image="" alt="Emmental" class="cardStyle">
   <div class="card-actions"  >
   <p class="headerStyle">
-  Login Ing Web
+ 
   <div class="horizontal justified">
             <iron-form id="loginForm">
               <form >
-                  <paper-input name="toAccount" label="From Account" value="{{fromAccount}}" required auto-validate  error-message="Please Account No "></paper-input>
-                  <paper-input name="toAccount"  label="To Account" value="{{toAccount}}" required auto-validate error-message="Please Destination Account No "></paper-input>
-                  <paper-input type="number" name="transferAmount"  label="Amount To Transfer" value="{{transferAmount}}" required auto-validate error-message="Please Enter TransferAmoun "></paper-input>
+         
+                  <paper-input name="toAccount" label="From Account" value="{{_getAccountDetails()}}" required auto-validate disabled error-message="Please Account No "></paper-input>
+                <!--  <paper-input name="toAccount"  label="To Account" value="{{toAccount}}" required auto-validate error-message="Please Destination Account No "></paper-input>-->
+                  <paper-dropdown-menu label="Select Benificiary" on-iron-select="_handleChangeSelect">
+                  <paper-listbox slot="dropdown-content" >
+                  <template is="dom-repeat" items={{toAccountListArray}} >
+                  <paper-item value={{item.accountId}}>{{item.accountHolderName}}</paper-item>
+                  </template>
+                  
+                
+                  </paper-listbox>
+           </paper-dropdown-menu>
+
+           
+                  <paper-input type="text" name="transferAmount"  label="Amount To Transfer" value="{{transferAmount}}"  required auto-validate error-message="Please Enter TransferAmoun "></paper-input>
                   <iron-autogrow-textarea rows="4" lable="Comment"  value="comment" placeholder="Please Comments.."></iron-autogrow-textarea>
                 <h4>Date| <small>Today</small></h4>
                   <paper-button raised >
@@ -265,7 +311,7 @@ Login Ing Web
      
 <template is="dom-repeat" id="employeeList"   items="{{loggedInUsraccountDetailNTrans}}" style="padding:2%">
 <div style="margin:20px;color:black">
-        <div>Transaction Id | <span><a href="/#" data-item$="item" id="{{loggedInUsraccountDetailNTrans.transactionId}}"  on-click="_currentTranactionDetails">{{item.transactionId}}</a></span></div>
+        <div>Transaction Id | <span><div on-click="_currentTranactionDetails" style="text-decoration: underline; color:blue"><a data-item$="{{item}}" id="{{loggedInUsraccountDetailNTrans.transactionId}}"  >{{item.transactionId}}</a></div></span></div>
         <div>TransferAmount | <span>{{item.transferAmount}}</span></div>
         <div>Comment  | <span>{{item.comment}}</span></div>
 
